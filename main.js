b07a832499b205d47833eb1a0e3e003cb8e8687e
Curtis Rueden <ctrueden@wisc.edu>
2018-05-16 22:27:09 -0700

    Remove early debugging statement

--- main.js
+++ main.js
@@ -130,8 +130,5 @@ app.on('activate', function () {
   }
 })
 
-console.log('hi')
-
-
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
