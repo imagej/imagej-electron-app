96f12af0641e9aff6f276e13f88fff7127a560a9
Curtis Rueden <ctrueden@wisc.edu>
2018-05-17 10:17:03 -0700

    Organize source code
    Loosely modeled after https://github.com/webtorrent/webtorrent-desktop.

--- src/main/index.js
+++ src/main/index.js
@@ -118,7 +118,7 @@ function createWindow () {
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(__dirname, '../renderer/index.html'),
     protocol: 'file:',
     slashes: true
   }))
