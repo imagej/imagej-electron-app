42ee943fed949d4789736ad85f629356429feb53
Eric Perlman <perlman@gmail.com>
2018-05-17 16:07:16 -0700

    Update rpath to include search path for 'npm start' (works) and standalone (doesn't).

--- fixrpath.js
+++ fixrpath.js
@@ -3,35 +3,63 @@
 
 // install_name_tool -rpath /Library/Java/JavaVirtualMachines/jdk1.8.0_112.jdk/Contents/Home/jre/lib/server jre1.8.0_172.jre/Contents/Home/lib/server/ node_modules/java/build/Release/nodejavabridge_bindings.node
 
+function deletepath(binary, rpath) {
+  if (process.platform === 'darwin') {
+    cmd = "install_name_tool -delete_rpath " + rpath + " " + binary;
+    process.stdout.write(cmd + '\n')
+    const { exec } = require('child_process');
+
+    exec(cmd, (err, stdout, stderr) => {
+      // the *entire* stdout and stderr (buffered)
+      console.log(`stdout: ${stdout}`);
+      console.log(`stderr: ${stderr}`);
+      if (err) {
+        console.error(err);
+        console.error("[May not be an error if previously run.]");
+      }
+    });
+  } else {
+    console.error("Unsupported platform: " + process.platform);
+    process.exit(1);
+  }
+}
+
+function addpath(binary, rpath) {
+  if (process.platform === 'darwin') {
+    cmd = "install_name_tool -add_rpath " + rpath + " " + binary;
+    process.stdout.write(cmd + '\n')
+    const { exec } = require('child_process');
+
+    exec(cmd, (err, stdout, stderr) => {
+      // the *entire* stdout and stderr (buffered)
+      console.log(`stdout: ${stdout}`);
+      console.log(`stderr: ${stderr}`);
+      if (err) {
+        console.error(err);
+        console.error("[May not be an error if previously run.]");
+      }
+    });
+  } else {
+    console.error("Unsupported platform: " + process.platform);
+    process.exit(1);
+  }
+}
+
 require('find-java-home')(function(err, home){
   if(err){
     console.error(err);
     process.exit(1);
   }
 
-  if (process.platform === 'darwin') {
-    var rpath = home + "/jre/lib/server";
-    var newrpath = "jre1.8.0_172.jre/Contents/Home/lib/server/"
-    var lib = "node_modules/java/build/Release/nodejavabridge_bindings.node"
-    cmd = "install_name_tool -rpath " + rpath + " " + newrpath + " " + lib;
-    process.stdout.write(home + '\n');
-    process.stdout.write(rpath + '\n');
-    process.stdout.write(cmd + '\n')
-    const { exec } = require('child_process');
+  var binary = "node_modules/java/build/Release/nodejavabridge_bindings.node";
+  /* Delete original rpath, which references the java used to compile */
+  deletepath(binary, home + "/jre/lib/server");
 
-    exec(cmd, (err, stdout, stderr) => {
-      if (err) {
-        console.error(err);
-        console.error("[May not be an error if previously run.]");
-        return;
-      }
-      // the *entire* stdout and stderr (buffered)
-      console.log(`stdout: ${stdout}`);
-      console.log(`stderr: ${stderr}`);
-    });
-  } else {
-    console.error("Unsupported platform: " + process.platform);
-    process.exit(1);
-  }
+  /* Add reference to downloaded JRE */
+  addpath(binary, "jre1.8.0_172.jre/Contents/Home/lib/server/");
+
+  addpath(binary, "Contents/Resources/app/jre1.8.0_172.jre/Contents/Home/lib/server");
 
 });
+
+
